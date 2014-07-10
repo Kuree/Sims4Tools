42b8240529db002704ff11bce7b70cd673daae43
ChaosMageX <ChaosMageX@users.noreply.github.com>
2014-07-10 04:14:16 -0500

    + Added missing build.csproj and renamed it buildAll.csproj to (hopefully) avoid GIT from automatically ignoring it.

---
+++ s4pi/buildAll/TestHarness.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace build
+{
+    static class TestHarness
+    {
+        [STAThread]
+        static int Main(params string[] args)
+        {
+            System.Windows.Forms.CopyableMessageBox.Show(
+                "Hello world\n\n\n\nAnd some more text that goes along a bit" +
+                "And some more text that goes along a bit " +
+                "And some more text that goes along a bit " +
+                "And some more text that goes along a bit " +
+                "And some more text that goes along a bit " +
+                "And some more text that goes along a bit " +
+                "And some more text that goes along a bit " +
+                "And some more text that goes along a bit " +
+                "And some more text that goes along a bit " +
+                "And some more text that goes along a bit " +
+                "And some more text that goes along a bit " +
+                "And some more text that goes along a bit " +
+                "And some more text that goes along a bit " +
+                "And some more text that goes along a bit " +
+                "And some more text that goes along a bit " +
+                "And some more text that goes along a bit " +
+                "And some more text that goes along a bit " +
+                "And some more text that goes along a bit " +
+                "And some more text that goes along a bit " +
+                "And some more text that goes along a bit " +
+                "And some more text that goes along a bit " +
+                "And some more text that goes along a bit " +
+                "And some more text that goes along a bit " +
+                ""
+                , "Caption", CopyableMessageBoxButtons.OK, CopyableMessageBoxIcon.Stop);
+            System.Windows.Forms.CopyableMessageBox.Show("Hello world");
+            System.Windows.Forms.CopyableMessageBox.Show("Hello world\n\n\n\nAnd some more text that goes along a bit");
+            System.Windows.Forms.CopyableMessageBox.Show("Hello world", "Caption", CopyableMessageBoxButtons.OK, CopyableMessageBoxIcon.Stop);
+            System.Windows.Forms.CopyableMessageBox.Show("Hello world\n\n\n\nAnd some more text that goes along a bit", "Caption", CopyableMessageBoxButtons.OK, CopyableMessageBoxIcon.Stop);
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
