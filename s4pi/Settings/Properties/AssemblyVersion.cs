3d04bfcb36f08a596876f1fad58e0ee852515f28
Kuree <xmlc001@gmail.com>
2014-07-10 16:03:39 -0400

    Took the broken wrapper away from s4pe

--- s4pi/Settings/Properties/AssemblyVersion.cs
+++ s4pi/Settings/Properties/AssemblyVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-[assembly: AssemblyVersion("1407.10.0234.*")]
+[assembly: AssemblyVersion("1407.10.2000.*")]
