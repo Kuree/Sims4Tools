eb4e7edc41046f85343f363911a0b25df1394e81
cmarNYC <cmarinetti@nyc.rr.com>
2016-01-13 10:35:27 -0500

    Create Program.cs

---
+++ s4pe Helpers/RLESDDSHelper/Program.cs
@@ -0,0 +1,66 @@
+/***************************************************************************
+ *  Copyright (C) 2014 by Keyi Zhang                                       *
+ *  kz005@bucknell.edu                                                     *
+ *                                                                         *
+ *  This file is part of the Sims 4 Package Interface (s4pi)               *
+ *                                                                         *
+ *  s4pi is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  s3pi is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with s4pi.  If not, see <http://www.gnu.org/licenses/>.          *
+ *																		   *
+ *  Modified May 2015 by Camille Marinetti								   *
+ ***************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using s4pi.Helpers;
+using System.IO;
+using s4pi.ImageResource;
+
+namespace RLESDDSHelper
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+           if(args.Contains("/import"))
+           {
+               List<string> largs = new List<string>(args);
+               largs.Remove("/import");
+               s4pi.Helpers.RunHelper.Run(typeof(Import), largs.ToArray());
+           }
+           else if(args.Contains("/export"))
+           {
+               using (FileStream fs = new FileStream(args[1], FileMode.Open))
+               {
+                   using (SaveFileDialog save = new SaveFileDialog() { Filter = "DDS DXT5|*.dds" })
+                   {
+                       if (save.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                       {
+                           using (FileStream fs2 = new FileStream(save.FileName, FileMode.Create))
+                           {
+                               RLEResource r = new RLEResource(1, fs);
+                               r.ToDDS().CopyTo(fs2);
+                           }
+                       }
+                   }
+               }
+           }
+        }
+    }
+}
