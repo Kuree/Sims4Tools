18512f8b81f27ade2ac76cd6eb5697c642df701d
cmarNYC <cmarinetti@nyc.rr.com>
2016-01-18 14:24:25 -0500

    Fixed names

--- s4pe Helpers/RLESDDSHelper/Properties/AssemblyInfo.cs
+++ s4pe Helpers/RLESDDSHelper/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DMAPImageHelper")]
+[assembly: AssemblyTitle("RLESDDSHelper")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DMAPImageHelper")]
+[assembly: AssemblyProduct("RLESDDSHelper")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
