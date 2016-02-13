3bff6199d0c4914f1a40b53847d069a7f37f7f4a
pboxx <pbox@unera.se>
2016-02-13 15:37:56 +0100

    Update header

--- s4pi Wrappers/AnimationResources/ClipResourceHandler.cs
+++ s4pi Wrappers/AnimationResources/ClipResourceHandler.cs
@@ -4,18 +4,18 @@
  *                                                                         *
  *  This file is part of the Sims 4 Package Interface (s4pi)               *
  *                                                                         *
- *  s3pi is free software: you can redistribute it and/or modify           *
+ *  s4pi is free software: you can redistribute it and/or modify           *
  *  it under the terms of the GNU General Public License as published by   *
  *  the Free Software Foundation, either version 3 of the License, or      *
  *  (at your option) any later version.                                    *
  *                                                                         *
- *  s3pi is distributed in the hope that it will be useful,                *
+ *  s4pi is distributed in the hope that it will be useful,                *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
  *  GNU General Public License for more details.                           *
  *                                                                         *
  *  You should have received a copy of the GNU General Public License      *
- *  along with s3pi.  If not, see <http://www.gnu.org/licenses/>.          *
+ *  along with s4pi.  If not, see <http://www.gnu.org/licenses/>.          *
  ***************************************************************************/
 using System.Collections.Generic;
 using s4pi.Interfaces;
