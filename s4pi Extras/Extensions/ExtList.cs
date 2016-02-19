f0449b46c1be3d1e253b48891eaee8de74ba95b6
Buzzler <brutal.buzzler@gmx.de>
2016-02-19 18:35:48 +0100

    Reformat ExtList and throw more meaningful exception on duplicate entries

--- s4pi Extras/Extensions/ExtList.cs
+++ s4pi Extras/Extensions/ExtList.cs
@@ -1,29 +1,34 @@
 ﻿/***************************************************************************
- *  Copyright (C) 2009, 2010 by Peter L Jones                              *
- *  pljones@users.sf.net                                                   *
+ *  Copyright (C) 2015, 2016 by the Sims 4 Tools development team          *
  *                                                                         *
- *  This file is part of the Sims 3 Package Interface (s3pi)               *
+ *  Contributors:                                                          *
+ *  Peter L. Jones (pljones@users.sf.net)                                  *
+ *  Buzzler                                                                *
  *                                                                         *
- *  s3pi is free software: you can redistribute it and/or modify           *
+ *  This file is part of the Sims 4 Package Interface (s4pi)               *
+ *                                                                         *
+ *  s4pi is free software: you can redistribute it and/or modify           *
  *  it under the terms of the GNU General Public License as published by   *
  *  the Free Software Foundation, either version 3 of the License, or      *
  *  (at your option) any later version.                                    *
  *                                                                         *
- *  s3pi is distributed in the hope that it will be useful,                *
+ *  s4pi is distributed in the hope that it will be useful,                *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
  *  GNU General Public License for more details.                           *
  *                                                                         *
  *  You should have received a copy of the GNU General Public License      *
- *  along with s3pi.  If not, see <http://www.gnu.org/licenses/>.          *
+ *  along with s4pi.  If not, see <http://www.gnu.org/licenses/>.          *
  ***************************************************************************/
-using System;
-using System.Collections.Generic;
-using System.IO;
-using s4pi.Extensions.Properties;
 
 namespace s4pi.Extensions
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using s4pi.Extensions.Properties;
+
     /// <summary>
     /// Provides a look-up from resource type to resource &quot;tag&quot; and file extension.
     /// The collection is read-only.
@@ -31,26 +36,48 @@ namespace s4pi.Extensions
     /// <seealso cref="TGIN"/>
     public class ExtList : Dictionary<string, List<string>>
     {
-        static ExtList e = null;
-        static ExtList() { e = new ExtList(); }
+        private static readonly ExtList e;
+
+        static ExtList()
+        {
+            e = new ExtList();
+        }
+
         /// <summary>
         /// A look-up from resource type to resource &quot;tag&quot; and file extension.
         /// The collection is read-only.
         /// </summary>
-        public static ExtList Ext { get { return e; } }
-
-        ExtList()
+        public static ExtList Ext
         {
-            StringReader sr = new StringReader(Resources.Extensions);
-            string s;
-            while ((s = sr.ReadLine()) != null)
+            get { return e; }
+        }
+
+        private ExtList()
+        {
+            StringReader reader = new StringReader(Resources.Extensions);
+            string line;
+            while ((line = reader.ReadLine()) != null)
             {
-                if (s.StartsWith(";")) continue;
-                List<string> t = new List<string>(s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
-                if (t.Count < 2) continue;
-                string t0 = t[0];
-                t.RemoveAt(0);
-                this.Add(t0, t);
+                if (line.StartsWith(";", StringComparison.Ordinal))
+                {
+                    continue;
+                }
+
+                List<string> parts = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                if (parts.Count < 2)
+                {
+                    continue;
+                }
+
+                string key = parts[0];
+                if (this.ContainsKey(key))
+                {
+                    string message = string.Format("ExtList already contains entry for key '{0}'", key);
+                    throw new InvalidOperationException(message);
+                }
+
+                parts.RemoveAt(0);
+                this.Add(key, parts);
             }
         }
 
